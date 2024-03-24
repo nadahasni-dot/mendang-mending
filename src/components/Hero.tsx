bf389131b716a6eb9133300cef6c0916c33b457e
Nada Hasni <nadasthing@gmail.com>
2024-03-24 14:35:30 +0700

    feat: adjust wording hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
         Bandingkan Sesuatu dengan Bantuan AI
       </h2>
       <p className="max-w-2xl text-sm md:text-base dark:text-slate-300 text-slate-600">
-        Bagi kamu kamu kaum mendang mending yang suka membandingkan sesuatu,{" "}
+        Bagi kamu kamu <span className="italic underline">"Kaum mendang-mending"</span> yang suka membandingkan sesuatu,{" "}
         <span className="font-semibold text-black dark:text-white">
           Mendang Mending
         </span>{" "}
